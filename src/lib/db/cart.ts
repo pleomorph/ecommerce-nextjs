79e047b0666ee54829b975c944c21bc1e04f7096
pleomorph <pleomorphed@gmail.com>
2023-11-14 20:34:02 +0000

    User login

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/dist/client/components/headers";
 import { prisma } from "./prisma";
 import { Cart, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
   include: { items: { include: { product: true } } };
@@ -16,13 +18,25 @@ export type ShoppingCart = CartWithProducts & {
 };
 
 export async function getCart(): Promise<ShoppingCart | null> {
+  const session = await getServerSession(authOptions);
+
   const localCartId = cookies().get("localCartId")?.value;
-  const cart = localCartId
-    ? await prisma.cart.findUnique({
-        where: { id: localCartId },
-        include: { items: { include: { product: true } } }, // Retrieve Item and Product information from db
-      })
-    : null; // If there is not localCartId, return null
+
+  let cart: CartWithProducts | null = null;
+
+  if (session) {
+    cart = await prisma.cart.findFirst({
+      where: { userId: session.user.id },
+      include: { items: { include: { product: true } } },
+    });
+  } else {
+    cart = localCartId
+      ? await prisma.cart.findUnique({
+          where: { id: localCartId },
+          include: { items: { include: { product: true } } }, // Retrieve Item and Product information from db
+        })
+      : null; // If there is not localCartId, return null
+  }
 
   if (!cart) {
     return null;
@@ -39,13 +53,23 @@ export async function getCart(): Promise<ShoppingCart | null> {
 }
 
 export async function createCart(): Promise<ShoppingCart> {
-  const newCart = await prisma.cart.create({
-    data: {},
-  });
+  const session = await getServerSession(authOptions);
 
-  // Store id of cart in browser cookie
-  // In production, you would want to encrypt the Cart Id so users cannot guess other Cart Ids
-  cookies().set("localCartId", newCart.id);
+  let newCart: Cart;
+
+  if (session) {
+    newCart = await prisma.cart.create({
+      data: { userId: session.user.id },
+    });
+  } else {
+    newCart = await prisma.cart.create({
+      data: {},
+    });
+
+    // Store id of cart in browser cookie
+    // In production, you would want to encrypt the Cart Id so users cannot guess other Cart Ids
+    cookies().set("localCartId", newCart.id);
+  }
 
   return {
     ...newCart,
